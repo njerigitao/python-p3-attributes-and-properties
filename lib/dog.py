5b627ba0b7bff283051ce9b2e299195df298f0c3
AGNESGITAU <agnesgitau40@gmail.com>
2024-05-24 13:16:21 +0300

    Define name property

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,15 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Bosco"):
+        self.name = name
+
+        def  get_name(self):
+            return self.name
+        def set_name(self, name):
+            if isinstance(name, str) and 1 <= len(name) <= 25:
+                self.name = name
+            else:
+                print("Name must be string between 1 and 25 characters.")
+
+
